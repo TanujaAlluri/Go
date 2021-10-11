acd710467ff65a65c365ee7a11ce9f6d938a6e35
Tanuja Alluri <altanuja@thoughtworks.com>
2021-10-11 17:20:49 +0530

    variable declaration

--- cards/main.go
+++ cards/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var card string = "Ace of spades"
+	// var card string = "Ace of spades"
+	card := "Ace of spades"
+	card = "Five of diamonds"
+
 	fmt.Println(card)
 }
