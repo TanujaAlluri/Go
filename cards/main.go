05035f8997d48186f73dcd197a73ff482c9ac795
Tanuja Alluri <altanuja@thoughtworks.com>
2021-10-11 18:11:49 +0530

    functions and return types

--- cards/main.go
+++ cards/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of spades"
-	card := "Ace of spades"
-	card = "Five of diamonds"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of diamonds"
 }
