b1698dfe6cca5b469ca5740eb965e5b6f121751f
Tanuja Alluri <altanuja@thoughtworks.com>
2021-10-11 19:19:26 +0530

    creating new deck, slice with range, func with multiple return values

--- cards/deck.go
+++ cards/deck.go
@@ -5,10 +5,28 @@ import "fmt"
 // create a new type of 'deck' which is a new slice of string
 type deck []string
 
-// receiver: setsup methods on a type,
-// so that the method is accessible to all variables of that type
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuites := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suite := range cardSuites {
+		for _, value := range cardValues {
+			cards = append(cards, value+"of"+suite)
+		}
+	}
+
+	return cards
+}
+
+// receiver
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
 }
--- cards/main.go
+++ cards/main.go
@@ -1,14 +1,8 @@
 package main
 
 func main() {
-	cards := deck{"Ace of spades", newCard()}
-	newCards := append(cards, "Six of Spades")
-
-	cards.print()
-
-	newCards.print()
-}
-
-func newCard() string {
-	return "Five of diamonds"
+	cards := newDeck()
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	remainingDeck.print()
 }
