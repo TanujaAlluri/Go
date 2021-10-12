c93a490a681e81102852b938e7c103200d3f1a01
Tanuja Alluri <altanuja@thoughtworks.com>
2021-10-12 18:56:25 +0530

    embedded structs

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,30 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	// contact   contactInfo
+	contactInfo
 }
 
 func main() {
-	// alex := person{"Alex", "Anderson"}
-	// alex := person{firstName: "Alex", lastName: "Anderson"}
-	var alex person
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		// contact: contactInfo{
+		// 	email:   "jim@email.com",
+		// 	zipCode: 987654,
+		// },
+		contactInfo: contactInfo{
+			email:   "jim@email.com",
+			zipCode: 987654,
+		},
+	}
+	fmt.Printf("%+v", jim)
 }
