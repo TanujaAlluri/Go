4f5d626881ff96a7f72cc1731c4c209abf43325a
Tanuja Alluri <altanuja@thoughtworks.com>
2021-10-13 16:55:36 +0530

    interfaces - problem without interfaces

---
+++ interfaces/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hello there!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
