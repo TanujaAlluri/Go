464ff84ed66e3731fc8f9b5f0162721ac8424771
Tanuja Alluri <altanuja@thoughtworks.com>
2021-10-18 20:15:05 +0530

    Solving :  'loop variable l captured by func literal' warning msg

--- channels/main.go
+++ channels/main.go
@@ -26,35 +26,56 @@ func main() {
 	// }
 
 	for l := range c { // infinite loop
-		go func() {
+		// go func() {
+		// 	time.Sleep(5 * time.Second)
+		// 	checkLink(l, c)
+		// }()
+
+		/* Line 31
+		warning message: loop variable l captured by func literal.
+		This essentially means that we are referencing loop variable l
+		in both main routine and inside child routine.
+
+		--------------------------------
+		address	|	value
+		---------------------------------
+		100			|	google.com(value of l) <--------------main routine
+		101			|				...											|
+		102			|				...											|---------child routine
+
+		For example, first we invoked all 5 website's checklink func.
+		We now got response, first from amazon, and we pushed the url to channel.
+		Then the value of 'l' is now 'Amazon'.
+		The main routine waits for 5 sec, and creates a new Amazon child routine.
+		And the Amazon child routine is waiting on a blocking http request.
+
+		Meanwhile, Google routine finished, and the url is pushed.
+		The new value of 'l' is 'Google'.
+		At this point, if we reference 'l' in Amazon routine, that was spun
+		the value would be 'Google' instead of 'Amazon'.
+
+		Since both main routine and child routine are pointing to the same address.
+		*/
+
+		go func(link string) {
 			time.Sleep(5 * time.Second)
-			checkLink(l, c)
-			/*
-				warning message: loop variable l captured by func literal.
-				This essentially means that we are referencing loop variable l
-				in both main routine and inside child routine.
+			checkLink(link, c)
+		}(l)
 
-				--------------------------------
-				address	|	value
-				---------------------------------
-				100			|	google.com(value of l) <--------------main routine
-				101			|				...											|
-				102			|				...											|---------child routine
+		/*
+			Since GO in 'Pass by value', we would be passing values
+			accross routines as arguments.
+			This make sure that l is passed as a copy
+			and the 'l' in main routine can change as much as it wants,
+			without the reference changing in child routine.
 
-				For example, first we invoked all 5 website's checklink func.
-				We now got response, first from amazon, and we pushed the url to channel.
-				Then the value of 'l' is now 'Amazon'.
-				The main routine waits for 5 sec, and creates a new Amazon child routine.
-				And the Amazon child routine is waiting on a blocking http request.
-
-				Meanwhile, Google routine finished, and the url is pushed.
-				The new value of 'l' is 'Google'.
-				At this point, if we reference 'l' in Amazon routine, that was spun
-				the value would be 'Google' instead of 'Amazon'.
-
-				Since both main routine and child routine are pointing to the same address.
-			*/
-		}()
+			--------------------------------
+			address	|	value
+			---------------------------------
+			100			|	google.com(value of l from main routine) <--------------main routine
+			101			|				...
+			102			|	google.com(value of link passed as an arg to func literal)<--child routine
+		*/
 	}
 }
 
