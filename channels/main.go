648dfd0987fe0242e34ad3ed7d679dff6e6b8e97
Tanuja Alluri <altanuja@thoughtworks.com>
2021-10-18 19:12:22 +0530

    Channels&Routines - solving partially using channels

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,21 @@ func main() {
 		"http://golang.org",
 	}
 
+	c := make(chan string)
+
 	for _, url := range urls {
-		go checkLink(url)
+		go checkLink(url, c)
 	}
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "is down")
+		c <- "Might be down I think"
 		return
-	} else {
-		fmt.Println(link, "is up")
 	}
+	fmt.Println(link, "is up")
+	c <- "Yep its up"
 }
